546bac04a0d1f0ca396250e47af88f7307d6f1d9
reis <kklemre@gmail.com>
2019-11-16 21:06:53 +0200

    banner hazırlandı

--- _assets/js/custom/kucuk-adimlar.js
+++ _assets/js/custom/kucuk-adimlar.js
@@ -1,1 +1,26 @@
-console.log('test');
+var kucukAdimlar = {
+	swiper: {
+		animate: function(){
+
+		},
+		$target: $(".swiper .owl-carousel"),
+		_controller: function(){
+			var $this = this.$target;
+			$this.owlCarousel({
+			    loop:true,
+			    nav:true,
+			    items: 1,
+			});
+		},
+		init: function(){
+			this._controller();
+		},
+	},
+	init: function(){
+		this.swiper.init();
+	},
+};
+
+$(document).ready(function(){
+	kucukAdimlar.init();
+});
