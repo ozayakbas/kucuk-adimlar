5fabf2f98938c64aa5fd2c5882180ca016d3198c
reis <kklemre@gmail.com>
2019-11-17 11:41:50 +0200

    banner animasyon eklendi

--- _assets/js/kucuk-adimlar.js
+++ _assets/js/kucuk-adimlar.js
@@ -3448,13 +3448,32 @@
 })(window.Zepto || window.jQuery, window, document);
 ;var kucukAdimlar = {
 	swiper: {
+		animateTime: 100,
 		animate: function(){
-
+			var initAnimate = function(){
+				var timeOut = 100;
+				setTimeout(function(){
+					kucukAdimlar.swiper.$target.find(".animated").removeClass("animated fadeIn");
+					var _img = kucukAdimlar.swiper.$target.find(".active .img-sub");
+					_img.each(function(){
+						var $this = $(this);
+						timeOut += kucukAdimlar.swiper.animateTime;
+						setTimeout(function(){
+							$this.addClass("animated fadeIn");
+						}, timeOut);								
+					});
+				}, 200);
+			};
+			initAnimate();
+			this.$banner.on('changed.owl.carousel', function(event) {
+				initAnimate();
+			});
 		},
+		$banner: null,
 		$target: $(".swiper .owl-carousel"),
 		_controller: function(){
 			var $this = this.$target;
-			$this.owlCarousel({
+			this.$banner = $this.owlCarousel({
 			    loop:true,
 			    nav:true,
 			    items: 1,
@@ -3462,6 +3481,7 @@
 		},
 		init: function(){
 			this._controller();
+			this.animate();
 		},
 	},
 	init: function(){
